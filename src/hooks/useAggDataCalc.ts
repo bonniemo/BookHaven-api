4e45acfdc82992ac88fae336a8e6fc4d51106979
Bonnie <moller.bon@gmail.com>
2024-05-16 22:58:00 +0200

    more aggData utils

--- src/hooks/useAggDataCalc.ts
+++ src/hooks/useAggDataCalc.ts
@@ -1,30 +1,25 @@
 import { useContext } from "react";
 import { GlobalContext } from "../state/GlobalStateContext";
-import { calculateTotal } from "../utils/Utils";
+import {
+  calcuateAverage,
+  calculateTotal,
+  findHighestRatedBook,
+  findLongestReadBook,
+} from "../utils/Utils";
 
 export const useAggDataCalc = () => {
   const { state } = useContext(GlobalContext);
   const readBooks = state.readBooks.length;
   const numOfFavAuthors = state.favouriteAuthors.length;
   const numOffFavBooks = state.favouriteBooks.length;
-
   const readPages = calculateTotal(state.readBooks, (book) =>
     Number(book.userNumPages)
   );
-
-  const totalRating = calculateTotal(state.readBooks, (book) =>
-    Number(book.userRating)
+  const averageBookRating = calcuateAverage(
+    state.readBooks.map((book) => Number(book.userRating))
   );
-
-  const averageBookRating = totalRating / state.readBooks.length;
-
-  const highestRatedBook = state.readBooks.reduce((highestRated, book) => {
-    return book.userRating > highestRated.userRating ? book : highestRated;
-  }, state.readBooks[0]);
-
-  const longestReadBook = state.readBooks.reduce((longestBook, book) => {
-    return book.userNumPages > longestBook.userNumPages ? book : longestBook;
-  }, state.readBooks[0]);
+  const highestRatedBook = findHighestRatedBook(state.readBooks);
+  const longestReadBook = findLongestReadBook(state.readBooks);
 
   return {
     readBooks,
--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,7 +1,29 @@
-import { ReadBook } from "../types/Types"
+import { ReadBook } from "../types/Types";
 
-export type CalcTotalType = (book: ReadBook) => number
+export type CalcTotalType = (book: ReadBook) => number;
 
-export const calculateTotal = (books: ReadBook[], getValue: CalcTotalType): number => {
-    return books.reduce((total, book) => total + getValue(book), 0)
-}
+export const calculateTotal = (
+  books: ReadBook[],
+  getValue: CalcTotalType
+): number => {
+  return books.reduce((total, book) => total + getValue(book), 0);
+};
+
+export const calcuateAverage = (values: number[]): number => {
+  const sum = values.reduce((total, value) => total + value, 0);
+  return sum / values.length;
+};
+
+export const findHighestRatedBook = (books: ReadBook[]): ReadBook => {
+  return books.reduce(
+    (highestRated, book) =>
+      book.userRating > highestRated.userRating ? book : highestRated,
+    books[0]
+  );
+};
+
+export const findLongestReadBook = (books: ReadBook[]): ReadBook => {
+  return books.reduce((longestBook, book) => {
+    return book.userNumPages > longestBook.userNumPages ? book : longestBook;
+  }, books[0]);
+};
