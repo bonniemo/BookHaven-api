093bd97a86b1c43161b9667f3777777a44eec69b
Bonnie <moller.bon@gmail.com>
2024-05-16 23:09:12 +0200

    moved type into function

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,10 +1,8 @@
 import { ReadBook } from "../types/Types";
 
-export type CalcTotalType = (book: ReadBook) => number;
-
 export const calculateTotal = (
   books: ReadBook[],
-  getValue: CalcTotalType
+  getValue: (book: ReadBook) => number
 ): number => {
   return books.reduce((total, book) => total + getValue(book), 0);
 };
